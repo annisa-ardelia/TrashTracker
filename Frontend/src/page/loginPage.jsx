ba3dcd40c76a04fddd11f9edd27a8ca74723001a
Zaidan <rizqizaidan03@gmail.com>
2024-11-11 20:46:01 +0700

    Membuat login page

--- Frontend/src/page/loginPage.jsx
+++ Frontend/src/page/loginPage.jsx
@@ -1,9 +1,26 @@
+import style from "../style";
+import Navbar from "../components/Navbar";
+import Login from "../components/login";
+// import Footer from "../components/Footer";
+
 const LoginPage = () => {
     return (
-        <div>
-            <h1>Login Page</h1>
+        <div className="bg-primary w-full relative pt-9">
+            <Navbar />
+
+            <div className={`bg-primary${style.flexStart} w-full h-screen flex flex-row justify-center items-center gap-24`}>
+                <div className={`${style.boxWidth}`}>
+                    <Login />
+                </div>
+            </div>
+
+            <div className={`bg-primary ${style.paddingX} ${style.flexCenter} flex-col md:absolute md:bottom-0 w-screen`}>
+                <div className={`${style.boxWidth}`}>
+                    {/* <Footer /> */}
+                </div>
+            </div>
         </div>
-    );
-}
+    )
+};
 
 export default LoginPage;
